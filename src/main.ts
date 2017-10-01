61ce34837bb8679b96569babe861afee8fbd83c4
Ninh Doan <uitnetwork@gmail.com>
2017-10-01 23:30:10 +0800

    Integrated with Google Cloud Storage

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
 'use strict';
+import { Request, Response } from 'express';
+import * as Storage from '@google-cloud/storage';
 
-export function http(request, response) {
+export function http(request: Request, response: Response) {
+
+    console.log(`Base URL is: ${request.baseUrl}`);
+    let googleStorage = Storage({
+        projectId: 'testProjectId'
+    });
+    googleStorage.createBucket('testBucket')
+        .then(() => console.log(`Bucket Created`))
+        .catch(err => console.log(err));
     response.status(200).send('Hello World From Image Function using TypeScript!');
 }
